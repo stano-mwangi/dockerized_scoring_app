85f31d5d1258c8b1ba8a967b257fdab3b21083fc
Stan <stanley@stanley.justart.com>
2025-05-25 20:12:09 +0300

    All untracked files and updates

--- docker/src/includes/db.php
+++ docker/src/includes/db.php
@@ -1,12 +1,18 @@
 <?php
-$dsn = 'sqlite:/var/www/html/src/database/scoring_app.sqlite';
+$host = 'db';
+$db   = 'scoring_app';
+$user = 'lampuser';
+$pass = 'lamp123';
+$charset = 'utf8mb4';
+$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
 $options = [
     PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
 ];
-
 try {
-    $pdo = new PDO($dsn, null, null, $options);
+    $pdo = new PDO($dsn, $user, $pass, $options);
 } catch (PDOException $e) {
     die("Database connection failed: " . $e->getMessage());
 }
+
+
 ?>
